1d840d18c576321726a4d0c7795888d97cb50f82
Thant713 <59516322+Thant713@users.noreply.github.com>
2026-08-18 16:58:04 -0400

    feat: add menu system for item management
    - Add main menu (Adjust items / View items)
    - Add adjust submenu (Add / Edit / Remove)
    - Add view submenu (View existing / Update existing)

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	for {
+		fmt.Println("Choose mode: 1) Adjust items  2) View items")
+		choice := getInput("> ")
+
+		switch choice {
+		case 1:
+			adjustMenu()
+		case 2:
+			viewMenu()
+		default:
+			fmt.Println("Invalid choice")
+		}
+	}
+}
+
+func adjustMenu() {
+	fmt.Println("1) Add  2) Edit  3) Remove")
+	choice := getInput("> ")
+
+	switch choice {
+	case 1:
+		item := Item{}
+		item.AddItem()
+	case 2:
+		// edit - later
+	case 3:
+		// remove - later
+	default:
+		fmt.Println("Invalid choice")
+	}
+}
+
+func viewMenu() {
+	fmt.Println("1) View existing items  2) Update existing items")
+	choice := getInput("> ")
+
+	switch choice {
+	case 1:
+		// view - later
+	case 2:
+		// update - later
+	default:
+		fmt.Println("Invalid choice")
+	}
 }
