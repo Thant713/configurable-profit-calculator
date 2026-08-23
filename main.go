fd2f2bdf34d3c8ab5d4646002ba5d0f8890c056f
Thant713 <59516322+Thant713@users.noreply.github.com>
2026-08-22 22:05:54 -0400

    main.go: loop over all 4 IDs, fetch and print prices

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+)
 
+func main() {
+	items := []int{goldBarID, diamondNeckID, diamondID, bondID}
+	for _, item := range items {
+		bothIds, err := fetchItemPrice(item)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(item, bothIds)
+	}
 }
